bdf5997fafa59b8cd191ea51d657eef6aa9b836f
raspimeteo <jjczon@gmail.com>
2020-05-08 15:52:24 -0500

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,5 @@
-require 'pry'
 def selects_all_female_bears_return_name_and_age
   'SELECT name, age FROM bears WHERE gender = "F";'
-  # binding.pry
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -21,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  'SELECT color,  COUNT(color) AS most_color FROM bears GROUP BY color ORDER BY most_color DESC LIMIT 1;'
+  'SELECT color, COUNT(color) AS most_color FROM bears GROUP BY color ORDER BY most_color DESC LIMIT 1;'
 end
 
 def counts_number_of_bears_with_goofy_temperaments
